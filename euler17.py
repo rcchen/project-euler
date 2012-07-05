ebfa693e8924675d29e158f93f0fd21948c994c8
Roger Chen <rogerthechen@gmail.com>
2012-07-05 15:35:46 -0700

    Euler 17 is finally working

--- euler17.py
+++ euler17.py
@@ -1,30 +1,44 @@
-one = 3
-two = 3
-three = 5
-four = 4
-five = 4
-six = 3
-seven = 5
-eight = 5
-nine = 4
-ten = 3
-eleven = 6
-twelve = 6
-thirteen = 8
-fourteen = 8
-fifteen = 7
-sixteen = 7
-seventeen = 9
-eighteen = 8
-nineteen = 8
-twenty = 6
-thirty = 6
-forty = 5
-fifty = 5
-sixty = 5
-seventy = 7
-eighty = 6
-ninety = 6
-hundred = 7
-thousand = 8
+# letter count corresponding to index
+ones = [0,3,3,5,4,4,3,5,5,4]
+teens = [3,6,6,8,8,7,7,9,8,8] #+10
+tens = [0,3,6,6,5,5,5,7,6,6]
 
+def HundredsCount(num):
+	return 7 + ones[num/100]
+
+def TensCount(num):
+	return tens[num/10] 
+
+def TeensCount(num):
+	return teens[num-10]
+
+def AssembledNumberCount(num):
+	temp = 0
+	if (num > 100):
+		temp += HundredsCount(num)
+		temp += 3
+	num = num % 100
+	if (num < 10):
+		return (temp + ones[num])
+	if (num < 20 and num >= 10):
+		return (temp + TeensCount(num))
+	temp += TensCount(num)
+	num = num % 10
+	return (temp + ones[num])
+
+def LetterCount(num):
+	if (num < 10):
+		return ones[num]
+	if (num >= 10 and num < 20):
+		return TeensCount(num)
+	if (num % 100 == 0):
+		return HundredsCount(num)
+	if (num % 10 == 0 and num < 100):
+		return TensCount(num)
+	return AssembledNumberCount(num)
+
+sum = 0
+for foo in range (1,1000):
+	sum += LetterCount(foo)
+sum += 11
+print "Total: " + str(sum)
