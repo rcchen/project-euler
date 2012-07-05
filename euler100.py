60bd89d2106e382831a219a3075d8b65758b0c74
Roger Chen <rogerthechen@gmail.com>
2012-07-05 15:45:25 -0700

    Deleted files are staged.

--- euler100.py
+++
@@ -1,6 +0,0 @@
-threshold = 1000000000000
-
-while 1:
-	for i in range (threshold/2,threshold):
-		if ((i/threshold)*((i-1)/(threshold-1)) == 0.5):
-			
--- euler26.py
+++
@@ -1,7 +0,0 @@
-low = -1000
-high = 1000
-
-
-for a in range(low,high):
-	for b in range(low,high):
-		
