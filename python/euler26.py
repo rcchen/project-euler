c68f6b751c81bd91796a3939a5553eb11a49a09e
Roger Chen <rogerthechen@gmail.com>
2012-07-10 11:02:34 -0700

    Euler26 now solved

--- python/euler26.py
+++ python/euler26.py
@@ -1,7 +1,24 @@
-low = -1000
-high = 1000
+def repeating_digits(num):
+	start = 10
+	count = 1
+	while (start % num != 1 and start % num != 0):
+		start = (start *10 )% num
+		count += 1
+	return count
 
+def is_prime(num):
+	for foo in range(2,num):
+		if (num%foo == 0):
+			return 0
+	return 1
 
-for a in range(low,high):
-	for b in range(low,high):
-		
+highest = 0
+thenumber = 0
+for x in range(3,1000):
+	if (is_prime(x)):
+		repeat = repeating_digits(x)
+		if (repeat > highest):
+			highest = repeat
+			thenumber = x
+
+print highest
